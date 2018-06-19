2f7a5c85d9299cfc3907e964b14fe10f4558ffd5
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-06-19 15:34:51 -0700

    Logomark

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Thaddeus Jordan" style={{ width: '48px'}} />
+            <img src={logo} alt="Thaddeus Jordan" style={{ width: '28px'}} />
           </figure>
         </Link>
       </div>
